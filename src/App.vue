97a010148028d80953aba3170f8745127c25baa4
rfumic <rokofumic@gmail.com>
2022-03-23 16:45:29 +0100

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-card width="1000" class="mx-auto mt-10 px-5">
+      <v-card-title>
+        <h1 class="display-1 mx-auto">{{ unos ? unos : "Promjeni me" }}</h1>
+      </v-card-title>
+      <v-text-field
+        v-model="unos"
+        label="Ime"
+        outlined
+        append-outer-icon="mdi-send"
+        @click:append-outer="getData"
+        clearable
+      ></v-text-field>
+
+      <v-data-table :headers="headers" :items="podaci"></v-data-table>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+
+  computed: {
+    headers() {
+      return [
+        { text: "Ime", value: "ime" },
+        { text: "Drzave i Vjerojatnosti", value: "drzava" },
+        { text: "Godine", value: "godine" },
+        { text: "Spol i Vjerojatnost", value: "spol" },
+      ];
+    },
+  },
+  data: () => ({
+    podaci: [],
+    unos: "",
+  }),
+  methods: {
+    async getData() {
+      console.log("Pozvano");
+      console.log(this.unos);
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
